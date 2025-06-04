ec46cd8b15951c4790fd313e03fb3d6c5c4f95df
NN <95127494+KaningNoppasin@users.noreply.github.com>
2025-06-05 00:38:54 +0700

    fix: responsive ui

--- src/form.tsx
+++ src/form.tsx
@@ -20,12 +20,15 @@ import {
 import {
     Form,
     FormControl,
-    FormDescription,
+    // FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
+import {
+    Textarea
+} from "@/components/ui/textarea"
 import {
     Input
 } from "@/components/ui/input"
@@ -59,7 +62,7 @@ const formSchema = z.object({
     date: z.coerce.date(),
     username: z.string().min(1).max(20),
     reviewNong: z.string().min(1).min(0).max(500),
-    selectFile: z.string()
+    // selectFile: z.string()
 });
 
 export default function MyForm() {
@@ -93,166 +96,176 @@ export default function MyForm() {
     }
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+        <div className="pl-5 pr-5">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
 
-                <div className="grid grid-cols-12 gap-4">
+                    <div className="grid grid-cols-4 sm:grid-cols-8 lg:grid-cols-12 gap-4">
 
-                    <div className="col-span-4">
+                        {/* className */}
+                        <div className="col-span-4">
 
-                        <FormField
-                            control={form.control}
-                            name="className"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>ClassName</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            placeholder="Python, Unity , etc."
+                            <FormField
+                                control={form.control}
+                                name="className"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>ClassName</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Python, Unity , etc."
 
-                                            type="text"
-                                            {...field} />
-                                    </FormControl>
-                                    <FormDescription>This is your public class name.</FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+                                                type="text"
+                                                {...field} />
+                                        </FormControl>
+                                        {/* <FormDescription>This is your public class name.</FormDescription> */}
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        {/* Date */}
+                        <div className="col-span-4">
+
+                            <FormField
+                                control={form.control}
+                                name="date"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Date</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "w-[240px] pl-3 text-left font-normal",
+                                                            !field.value && "text-muted-foreground"
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(field.value, "PPP")
+                                                        ) : (
+                                                            <span>Pick a date</span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        {/* <FormDescription>Your date .</FormDescription> */}
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        {/* username */}
+                        <div className="col-span-4">
+
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="NongNing ..."
+
+                                                type=""
+                                                {...field} />
+                                        </FormControl>
+                                        {/* <FormDescription>This is your public user name.</FormDescription> */}
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
                     </div>
 
-                    <div className="col-span-4">
+                    <FormField
+                        control={form.control}
+                        name="reviewNong"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>ReviewNong</FormLabel>
+                                <FormControl>
+                                    {/* <Input
+                                        placeholder="ReviewNong"
 
-                        <FormField
-                            control={form.control}
-                            name="date"
-                            render={({ field }) => (
-                                <FormItem className="flex flex-col">
-                                    <FormLabel>Date</FormLabel>
-                                    <Popover>
-                                        <PopoverTrigger asChild>
-                                            <FormControl>
-                                                <Button
-                                                    variant={"outline"}
-                                                    className={cn(
-                                                        "w-[240px] pl-3 text-left font-normal",
-                                                        !field.value && "text-muted-foreground"
-                                                    )}
-                                                >
-                                                    {field.value ? (
-                                                        format(field.value, "PPP")
-                                                    ) : (
-                                                        <span>Pick a date</span>
-                                                    )}
-                                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                                </Button>
-                                            </FormControl>
-                                        </PopoverTrigger>
-                                        <PopoverContent className="w-auto p-0" align="start">
-                                            <Calendar
-                                                mode="single"
-                                                selected={field.value}
-                                                onSelect={field.onChange}
-                                                initialFocus
-                                            />
-                                        </PopoverContent>
-                                    </Popover>
-                                    <FormDescription>Your date .</FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                    </div>
+                                        type=""
+                                        {...field} /> */}
+                                    <Textarea
+                                        placeholder="ReviewNong"
+                                        className="resize-none h-80"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                {/* <FormDescription>This is your public ReviewNong.</FormDescription> */}
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-                    <div className="col-span-4">
-
-                        <FormField
-                            control={form.control}
-                            name="username"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Username</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            placeholder="NongNing ..."
-
-                                            type=""
-                                            {...field} />
-                                    </FormControl>
-                                    <FormDescription>This is your public user name.</FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                    </div>
-
-                </div>
-
-                <FormField
-                    control={form.control}
-                    name="reviewNong"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>ReviewNong</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder="ReviewNong"
-
-                                    type=""
-                                    {...field} />
-                            </FormControl>
-                            <FormDescription>This is your public ReviewNong.</FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-
-                {/* <FormField
-                    control={form.control}
-                    name="selectFile"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Select File</FormLabel>
-                            <FormControl>
-                                <FileUploader
-                                    value={files}
-                                    onValueChange={setFiles}
-                                    dropzoneOptions={dropZoneConfig}
-                                    className="relative bg-background rounded-lg p-2"
-                                >
-                                    <FileInput
-                                        id="fileInput"
-                                        className="outline-dashed outline-1 outline-slate-500"
+                    {/* <FormField
+                        control={form.control}
+                        name="selectFile"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Select File</FormLabel>
+                                <FormControl>
+                                    <FileUploader
+                                        value={files}
+                                        onValueChange={setFiles}
+                                        dropzoneOptions={dropZoneConfig}
+                                        className="relative bg-background rounded-lg p-2"
                                     >
-                                        <div className="flex items-center justify-center flex-col p-8 w-full ">
-                                            <CloudUpload className='text-gray-500 w-10 h-10' />
-                                            <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-                                                <span className="font-semibold">Click to upload</span>
-                                                &nbsp; or drag and drop
-                                            </p>
-                                            <p className="text-xs text-gray-500 dark:text-gray-400">
-                                                SVG, PNG, JPG or GIF
-                                            </p>
-                                        </div>
-                                    </FileInput>
-                                    <FileUploaderContent>
-                                        {files &&
-                                            files.length > 0 &&
-                                            files.map((file, i) => (
-                                                <FileUploaderItem key={i} index={i}>
-                                                    <Paperclip className="h-4 w-4 stroke-current" />
-                                                    <span>{file.name}</span>
-                                                </FileUploaderItem>
-                                            ))}
-                                    </FileUploaderContent>
-                                </FileUploader>
-                            </FormControl>
-                            <FormDescription>Select a file to upload.</FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                /> */}
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
+                                        <FileInput
+                                            id="fileInput"
+                                            className="outline-dashed outline-1 outline-slate-500"
+                                        >
+                                            <div className="flex items-center justify-center flex-col p-8 w-full ">
+                                                <CloudUpload className='text-gray-500 w-10 h-10' />
+                                                <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                                                    <span className="font-semibold">Click to upload</span>
+                                                    &nbsp; or drag and drop
+                                                </p>
+                                                <p className="text-xs text-gray-500 dark:text-gray-400">
+                                                    SVG, PNG, JPG or GIF
+                                                </p>
+                                            </div>
+                                        </FileInput>
+                                        <FileUploaderContent>
+                                            {files &&
+                                                files.length > 0 &&
+                                                files.map((file, i) => (
+                                                    <FileUploaderItem key={i} index={i}>
+                                                        <Paperclip className="h-4 w-4 stroke-current" />
+                                                        <span>{file.name}</span>
+                                                    </FileUploaderItem>
+                                                ))}
+                                        </FileUploaderContent>
+                                    </FileUploader>
+                                </FormControl>
+                                <FormDescription>Select a file to upload.</FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    /> */}
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+        </div>
     )
 }
