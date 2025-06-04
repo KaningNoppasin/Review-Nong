30d23ac45b54e8c692ba344af0851a04189ac57a
NN <95127494+KaningNoppasin@users.noreply.github.com>
2025-06-05 00:12:10 +0700

    add: shadcn form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // import './App.css'
 
+import MyForm from "./form"
+
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      {/* <h1>Hello</h1> */}
+      <MyForm/>
     </>
   )
 }
