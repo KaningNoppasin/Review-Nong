c3a99dbc2b7d118f0b5e0c0ea3c5ce393328647d
NN <95127494+KaningNoppasin@users.noreply.github.com>
2025-06-22 12:08:29 +0700

    fix formSchema max length

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-    className: z.string().min(1).max(20),
+    className: z.string().min(1).max(40),
     date: z.coerce.date(),
     username: z.string().min(1).max(20),
-    topic: z.string().min(1).min(0).max(200),
+    topic: z.string().min(1).min(0).max(300),
     reviewNong: z.string().min(1).min(0).max(500),
 });
